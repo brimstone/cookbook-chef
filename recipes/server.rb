88e0d80b290ed255aff20fbc10d378f952525142
Matt <brimstone@the.narro.ws>
2014-02-25 21:43:43 -0500

    server: Adding checks for when running not in vagrant

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,11 +1,19 @@
+remote_file "/tmp/chef-server.deb" do
+	source "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.11-1.ubuntu.12.04_amd64.deb"
+	checksum "864e447580552a91b3de8be6c24efde2c5a6328a2b4dca8d5afa4347cb4b4868"
+	action :create_if_missing
+end
+
 dpkg_package "chef-server" do
-	source "/vagrant/packages/chef-server.deb"
+	source "/tmp/chef-server.deb"
 	action :install
 end
 
-execute "chef-server-ctl reconfigure"
+execute "/usr/bin/chef-server-ctl reconfigure"
 
-execute "cp /etc/chef-server/chef-validator.pem /vagrant/chef-validator.pem"
+execute "cp /etc/chef-server/chef-validator.pem /vagrant/chef-validator.pem" do
+	only_if { ::File.directory?("/vagrant") }
+end
 
 execute "cp /etc/chef-server/admin.pem /etc/chef/client.pem"
 
@@ -18,6 +26,10 @@ end
 
 execute "sleep 5"
 
-execute "knife cookbook -c /root/.chef/knife.rb upload -a -o /vagrant/cookbooks/"
+execute "knife cookbook -c /root/.chef/knife.rb upload -a -o /vagrant/cookbooks/" do
+	only_if { ::File.directory?("/vagrant") }
+end
 
-execute "knife role -c /root/.chef/knife.rb from file /vagrant/roles/*.json"
+execute "knife role -c /root/.chef/knife.rb from file /vagrant/roles/*.json" do
+	only_if { ::File.directory?("/vagrant") }
+end
